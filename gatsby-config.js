95112c45f15121941b9b4c5ecfdc04679d202a52
Saul Mirone <Saul-Mirone@outlook.com>
2021-08-03 16:09:32 +0000

    post new blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-twemoji-shortcut`,
         ],
       },
     },
