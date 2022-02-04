2ad79b3346030d82ca3a4a6dc5f413c3dd9f8030
Saul-Mirone <Saul-Mirone@users.noreply.github.com>
2022-02-04 04:08:08 +0000

    deploy: ba191535b29063e873aafb260801d20d4c1007bf

---
+++ component---src-pages-404-js-ce62c2ac6eafd3d7ab55.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[883],{2382:function(t,e,a){a.r(e);var n=a(9496),l=a(2779),s=a(3896);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ce62c2ac6eafd3d7ab55.js.map
